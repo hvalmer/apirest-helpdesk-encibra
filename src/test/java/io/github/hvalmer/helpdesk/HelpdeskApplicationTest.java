5b5ec16316bfa3c0232352aa74cab92c2fb391a6
hvalmer <hvalmer@gmail.com>
2023-09-12 23:05:34 -0300

    Implementação da Classe de Teste Java

---
+++ src/test/java/io/github/hvalmer/helpdesk/HelpdeskApplicationTest.java
@@ -0,0 +1,12 @@
+package io.github.hvalmer.helpdesk;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelpdeskApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
